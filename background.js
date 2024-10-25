921e3636376a43b141250d0290bb18c22cf7ca46
ms5914 <55366357+ms5914@users.noreply.github.com>
2024-10-25 01:00:29 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,19 +1,21 @@
 let visitedSites = new Set();
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url) {
+console.log("mahak");
+console.log(tab);
+if (changeInfo.status === 'complete' && tab.url) {
     const url = new URL(tab.url);
     // Skip chrome:// urls and other browser pages
     if (!url.protocol.startsWith('chrome')) {
-      visitedSites.add(url.hostname);
+      visitedSites.add(tab.url);
       chrome.storage.local.set({ 'sites': Array.from(visitedSites) });
-      
+
       // Store visit time
       const visit = {
-        url: url.hostname,
+        url: tab.url,
         timestamp: new Date().toISOString()
       };
-      
+
       chrome.storage.local.get('history', (data) => {
         const history = data.history || [];
         history.push(visit);
